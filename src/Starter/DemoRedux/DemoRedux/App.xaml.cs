533cb6fa3e7f14d34250d5c11f7a2b71a3c7f19c
jesulink2514 <jesus.angulo@outlook.com>
2018-09-14 00:55:06 -0500

    Finish touches

---
+++ src/Starter/DemoRedux/DemoRedux/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DemoRedux
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Starter/DemoRedux/DemoRedux/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DemoRedux.Todo.Actions;
+using DemoRedux.Todo.States;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using XLabs.Forms.Controls;
+
+namespace DemoRedux
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Toogle_ViewCompleted(object sender, EventArgs e)
+        {
+        }
+
+        private void NewTodo_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void Todo_CheckedChanged(object sender, XLabs.EventArgs<bool> e)
+        {
+        }
+    }
+}
